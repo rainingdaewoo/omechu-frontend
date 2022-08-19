c6c01d314882227324a84d0a2a045d5f863dd17f
김우중 <rainingdaewoo@gmail.com>
2022-08-19 18:23:24 +0900

    서버 주소 변경

--- src/data/httpAddress.js
+++ src/data/httpAddress.js
@@ -1,5 +1,5 @@
-const httpAddress = "http://localhost:5000";
-// const httpAddress = "http://43.200.20.53";
+//const httpAddress = "http://localhost:5000";
+ const httpAddress = "http://43.200.20.53";
 // LoginForm.js는 직접 변경 필요
   
 export default httpAddress;
